8866e381b87250570d15504b6710ff96bae69da9
Nazariy <nazariy.holovach2007@gmail.com>
2025-03-19 20:12:12 +0200

    Included the navbar scripts to the main TS file

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,4 +1,5 @@
 import '../scss/main.scss'; // Ensure this is imported
 
+import './breakpoint-handler'; // Import breakpoint handler
 
-import './breakpoint-handler' // Import breakpoint handler
+import  './components/navbar'; // Import navbar scripts
