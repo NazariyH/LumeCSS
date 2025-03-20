f28c1bd651419c3e648e722fe523f048918f446a
Nazariy <nazariy.holovach2007@gmail.com>
2025-03-20 12:47:19 +0200

    Added the accordion TS component and included it to the main TS file

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,5 +2,6 @@ import '../scss/main.scss'; // Ensure this is imported
 
 import './breakpoint-handler'; // Import breakpoint handler
 
-import  './components/navbar'; // Import navbar scripts
-import  './components/modal'; // Import modal scripts
+import './components/navbar'; // Import navbar scripts
+import './components/modal'; // Import modal scripts
+import './components/accordion'; // Import accordion scripts
