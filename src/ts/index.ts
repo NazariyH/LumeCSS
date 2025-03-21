b4cba29477cf9f442d5b7bab7140c7d650fe373e
Nazariy <nazariy.holovach2007@gmail.com>
2025-03-21 11:42:51 +0200

    Added the dropdown.ts component and included it in the main TS file

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,6 +2,7 @@ import '../scss/main.scss'; // Ensure this is imported
 
 import './breakpoint-handler'; // Import breakpoint handler
 
-import './components/navbar'; // Import navbar scripts
-import './components/modal'; // Import modal scripts
+import './components/dropdown'; // Import dropdown scripts
 import './components/accordion'; // Import accordion scripts
+import './components/modal'; // Import modal scripts
+import './components/navbar'; // Import navbar scripts
