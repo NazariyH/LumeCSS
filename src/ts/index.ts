ea6c40e11d0603be4e7cd172c9f30f0a308fb2cf
Nazariy <nazariy.holovach2007@gmail.com>
2025-03-22 22:01:31 +0200

    Added the toast TS component and included it to the main TS file

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -5,5 +5,6 @@ import './breakpoint-handler'; // Import breakpoint handler
 import './components/dropdown'; // Import dropdown scripts
 import './components/accordion'; // Import accordion scripts
 import './components/alert'; // Import alert scripts
+import './components/toast'; // Import toast scripts
 import './components/modal'; // Import modal scripts
 import './components/navbar'; // Import navbar scripts
