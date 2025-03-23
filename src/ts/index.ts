a39a50150cb639f3094536cd7ca6a8b736383d78
Nazariy <nazariy.holovach2007@gmail.com>
2025-03-23 12:14:31 +0200

    Added the tooltip TS file and included it to the main TS file

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -3,6 +3,7 @@ import '../scss/main.scss'; // Ensure this is imported
 import './breakpoint-handler'; // Import breakpoint handler
 
 import './components/dropdown'; // Import dropdown scripts
+import './components/tooltip'; // Import tooltip scripts
 import './components/accordion'; // Import accordion scripts
 import './components/alert'; // Import alert scripts
 import './components/toast'; // Import toast scripts
